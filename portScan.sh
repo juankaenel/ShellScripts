42188b0db23d40789cafdb461af71955623d202c
Juan Kaenel <kaenel10@gmail.com>
2020-12-21 00:44:36 -0500

    Port Scan

---
+++ portScan.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+trap ctrl_c INT
+
+function ctrl_c() {
+	echo -e "\n\n[*] Saliendo...\n"
+	tput cnorm; exit 0
+}
+
+tput civis; for port in $(seq 1 65535); do
+	timeout 1 bash -c "echo '' < /dev/tcp/10.10.10.209/$port" 2>/dev/null && echo "Puerto $port - Abierto" &
+done; wait
+
+tput cnorm
